75188067e7c3ebb299e78d433549f685038181f0
Szymon Mentel <mentel.szymon@gmail.com>
2015-01-21 09:49:06 +0100

    Add running tests for memetics

--- run_tests.sh
+++ run_tests.sh
@@ -25,20 +25,43 @@ run () {
     done
 }
 
+run_mems () {
+
+    for eval in $eval_memetics; do
+        for mut in $mut_memetics; do
+            for run in `seq 1 $run_repeat`; do
+                mkdir -p $output_root/$ops-$model-$cores-w$workers/ev-$eval/mt-$mut
+
+                echo "running $model in $rtime mlsecs with $workers skel workers on $core cores with $ops operators.."
+                echo "using $eval memetics for evaluation and $mut memetics for mutations"
+                logfile="emas_$rtime"`date +"-%s"`".log"
+
+                output_file=$output_root/$ops-$model-$cores-w$workers/ev-$eval/mt-$mut/$logfile
+                echo $output_file
+                erl -pa ebin -pa deps/*/ebin \
+                    -eval "emas:start($rtime,[{model, $model}, {skel_workers,$workers},{genetic_ops,$ops}, {genetic_ops_opts, [{mem_evaluation, $eval}, {mem_mutation, $mut}]}, {problem_size,50}])." \
+                    -run init stop -noshell > $output_file
+            done
+        done
+    done
+}
+
+
 
 output_dir="output"
-# rtime=120000
-rtime=2000
+rtime=600000 # 10 min
 
-# cores="1 2 4"
-cores="4"
-# run_repeat=3
-run_repeat=1
-skel_workers=4
-models="mas_skel mas_sequential mas_concurrent mas_hybrid"
-operators="labs_ops"
+eval_memetics="sdls"
+mut_memetics="rhmc tabu default_mem"
 
 
-output_root=$output_dir/tests
+cores=2
+run_repeat=20
+workers=2
+model="mas_skel" # mas_sequential mas_concurrent mas_hybrid"
+ops="labs_ops"
 
-run
+
+output_root=$output_dir
+
+run_mems
